8679b452861e16321bc68f3ee46edf39acfec9e8
amdudler <amir.duratovic@student.htldornbirn.at>
2025-12-03 11:21:36 +0100

    React Setup & React Struktur und Aufbau done

---
+++ React/4aWI_React_Demo/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Card from './components/Card';
+
+export default function App() {
+  return (
+    <div>
+      <h1>HELLO</h1>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
+}
