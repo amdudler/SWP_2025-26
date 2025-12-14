f720318a9dc22fd14530e6cef6150e97b9f133f2
amdudler <amir.duratovic@student.htldornbirn.at>
2025-12-14 17:05:18 +0100

    Tailwind Properties done

--- React/4aWI_React_Demo/src/App.tsx
+++ React/4aWI_React_Demo/src/App.tsx
@@ -1,15 +1,13 @@
-import './App.css'
 import Card from './components/Card';
 
 export default function App() {
   return (
     <div>
       <h1>HELLO</h1>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      <div className="grid grid-cols-2 w-80 gap-4"></div>
+      <Card title="Susi" description="Super Lehrerin"/>
+      <Card title="Hans" description="Super Typ"/>
+      <Card title="Sepp" description="Echt gut"/>
     </div>
   );
 }
--- React/4aWI_React_Demo/src/components/Card.tsx
+++ React/4aWI_React_Demo/src/components/Card.tsx
@@ -1,3 +1,15 @@
-export default function Card() {
-    return <div>I am a card</div>;
+type Props = {
+    title: string;
+    description: string;
+};
+
+export default function Card({ title, description }: Props) {
+    return (
+        <div className="border p-4">
+            <h2 className="font-bold">{title}</h2>
+            <p>{description}</p>
+        </div>
+    );
 }
+
+
